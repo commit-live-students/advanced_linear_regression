3e07f50a780b251747dfd3dec6f2004936f57f6f
ABHISHEK THOMBRE <ug201314001@iitj.ac.in>
2017-10-07 20:54:27 +0530

    Update test_q03_polynomial.py

--- q03_polynomial/tests/test_q03_polynomial.py
+++ q03_polynomial/tests/test_q03_polynomial.py
@@ -13,4 +13,6 @@ class TestPolynomial(TestCase):
     def test_polynomial(self):
         model = polynomial()
         prediction = model.predict(np.array([4, 5, 6, 7]).reshape(1, -1))
+        prediction1 = model.predict(np.array([10, 20, 30, 40]).reshape(1, -1))
         self.assertTrue(prediction, np.array([-50871.05760668]))
+        self.assertTrue(prediction1, np.array([-57995900.29157974]))
