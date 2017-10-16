35c0a3ba3dc2f5ca937d052bc87c842c353d693f
melviii100 <melvinjose1992@gmail.com>
2017-10-16 08:48:52 +0000

    Done

--- q06_cross_validation/build.py
+++ q06_cross_validation/build.py
@@ -10,9 +10,4 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 # Write your solution here
 def cross_validation(model, x, y ):
     scores = cross_val_score(model, x, y, scoring="neg_mean_squared_error", cv=5)
-    return scores
-
-from sklearn.linear_model import Ridge
-value = cross_validation(Ridge(alpha=0.1), X_train, y_train).mean()
-# -1778803314
-print value
+    return scores.mean()
