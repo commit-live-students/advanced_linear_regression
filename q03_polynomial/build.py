3389dbb6cc3dff6b67acf64e1816073921331c4a
melviii100 <melvinjose1992@gmail.com>
2017-10-16 08:43:46 +0000

    Done

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -4,7 +4,6 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
 import numpy as np
-
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
@@ -14,21 +13,11 @@ def polynomial(power=5, random_state=9):
     global X_train
     global y_train
 
+    np.random.RandomState(random_state)
+
     X_train =  X_train[['OverallQual','GrLivArea','GarageCars','GarageArea']]
-    rng = np.random.RandomState(random_state)
-    #power = 2 # due to memory error
-    poly = PolynomialFeatures(degree=power, include_bias=False)
-    linreg = LinearRegression(normalize=True)
-    new_X_train = poly.fit_transform(X_train)
-    linreg.fit(new_X_train, y_train)
-    print new_X_train.shape
-    new_x_test  = np.array([4, 5, 6, 7]).reshape(1, -1)
-    print new_x_test.shape
-    return
-    print linreg.predict(new_x_test)
-    return linreg
+    poly_model = make_pipeline(PolynomialFeatures(degree=power), LinearRegression())
+    #new_X_train = poly_model.fit_transform(X_train)
+    poly_model.fit(X_train, y_train)
 
-model = polynomial()
-
-#print(model.predict(np.array([4, 5, 6, 7]).reshape(1, -1)))
-new_x_test  = np.array([4, 5, 6, 7]).reshape(1, -1)
+    return poly_model
--- q06_cross_validation/build.py
+++ q06_cross_validation/build.py
@@ -2,11 +2,17 @@
 from sklearn.model_selection import cross_val_score
 import numpy as np
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
-
 np.random.seed(9)
+
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
 # Write your solution here
+def cross_validation(model, x, y ):
+    scores = cross_val_score(model, x, y, scoring="neg_mean_squared_error", cv=5)
+    return scores
 
-
+from sklearn.linear_model import Ridge
+value = cross_validation(Ridge(alpha=0.1), X_train, y_train).mean()
+# -1778803314
+print value
