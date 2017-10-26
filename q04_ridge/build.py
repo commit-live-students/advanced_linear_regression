c1880db6aa4a19ab33736993089176070b0acb18
sudharsanasai <Sudharsanasai@gmail.com>
2017-10-26 06:44:20 +0000

    Done

--- q04_ridge/build.py
+++ q04_ridge/build.py
@@ -9,6 +9,12 @@ np.random.seed(9)
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
-
+def ridge(alpha = 0.01):
+    model = Ridge(alpha=alpha,random_state=9,normalize=True)
+    model.fit(X_train,y_train)
+    y_pred_train = model.predict(X_train)
+    y_pred_test = model.predict(X_test)
+    rmse_train = mean_squared_error(y_pred_train,y_train)
+    rmse_test = mean_squared_error(y_pred_test,y_test)
+    return rmse_train**0.5,rmse_test**0.5
 # Write your solution here
-
