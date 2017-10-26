8ae2652ee6e3583d5ef8839b5ba14cee7e45707b
rameshgayam <ramesh.gayam@gmail.com>
2017-10-26 02:37:02 +0000

    Done

--- q04_ridge/build.py
+++ q04_ridge/build.py
@@ -1,14 +1,25 @@
+
 # Default imports
 from sklearn.linear_model import Ridge
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import PolynomialFeatures
+
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
-np.random.seed(9)
 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
+np.random.seed(9)
 
-# Write your solution here
-
+def ridge(alpha=0.01):
+    #code to implement l2 and to from
+    model = Ridge(alpha=alpha, normalize=True)
+    model.fit(X_train, y_train)
+    y_predicted_train = model.predict(X_train)
+    y_predicted_test = model.predict(X_test)
+    train_rmsc = np.sqrt(mean_squared_error(y_train, y_predicted_train))
+    test_rmsc = np.sqrt(mean_squared_error(y_test, y_predicted_test))
+    return train_rmsc, test_rmsc
