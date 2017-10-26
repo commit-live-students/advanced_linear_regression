c9f53ee0a6f5347af641b02587b85faf79706b78
sudharsanasai <Sudharsanasai@gmail.com>
2017-10-26 06:48:46 +0000

    Done

--- q05_lasso/build.py
+++ q05_lasso/build.py
@@ -9,5 +9,13 @@ np.random.seed(9)
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
+def lasso(alpha=0.01):
+    model = Lasso(alpha=alpha,random_state=9,normalize=True)
+    model.fit(X_train,y_train)
+    y_train_pred = model.predict(X_train)
+    y_test_pred = model.predict(X_test)
+    rmse_train = mean_squared_error(y_train_pred,y_train)**0.5
+    rmse_test = mean_squared_error(y_test_pred,y_test)**0.5
+    return rmse_train,rmse_test
 
 # Write your solution here
