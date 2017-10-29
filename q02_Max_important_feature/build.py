498b28d9d6b3d96d47336c8b7fe14c47c6f854fc
gaurav-barclays <gaurav.simply@gmail.com>
2017-10-29 16:27:56 +0000

    Done

--- q02_Max_important_feature/build.py
+++ q02_Max_important_feature/build.py
@@ -1,3 +1,4 @@
+# %load q02_Max_important_feature/build.py
 # Default imports
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 
@@ -6,4 +7,7 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your code here
-
+def Max_important_feature(data_set,target_variable,n=4):
+    x=data_set.corr().iloc[:-1,]
+    y=x[target_variable].sort_values().tail(n)
+    return y.index
