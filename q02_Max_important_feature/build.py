ad12d6593ec0eedaaf05700d5b441356151d945c
ashwinij9 <ashwinijoshi9793@gmail.com>
2017-10-30 04:22:59 +0000

    Done

--- q02_Max_important_feature/build.py
+++ q02_Max_important_feature/build.py
@@ -3,9 +3,10 @@ from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 import numpy as np
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
-target_var = "SalePrice"
-def Max_important_feature(data_set,target_var='SalePrice',n=4):
-    a =data_set.corr()
-    b=a["SalePrice"].sort_values(ascending = False)[1:(n+1)]
-    c=np.array(b.index).tolist()
-    return c
+
+def Max_important_feature(data_set, target_varilable='SalePrice', n=4):
+    d = {}
+    for col in data_set.columns.tolist():
+        d[col] = data_set[col].corr(data_set[target_varilable])
+    del d[target_varilable]
+    return np.array(sorted(d, key=d.get, reverse=True)[:n], dtype=object)
--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -3,11 +3,15 @@ from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
+import numpy as np
 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
-def ploynomial(power=5,random_state=9):
-    poly_model = make_pipeline(PolynomialFeatures(power),LinearRegression())
-    df1 = X_train[["OverallQual","GrLivArea","GarageCars","GarageArea"]]
-    model = poly_model.fit(df1,y_train)
-    return poly_model
+def polynomial(power=5, random_state=9):
+    global X_train
+    global y_train
+    np.random.RandomState(random_state)
+    X_train =  X_train[['OverallQual','GrLivArea','GarageCars','GarageArea']]
+    model = make_pipeline(PolynomialFeatures(degree=power), LinearRegression())
+    model.fit(X_train, y_train)
+    return model
