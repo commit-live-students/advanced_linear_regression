a9a5ee000ce07f2d1c9ee2425034ecb195c4077c
pritambhaladhare <pritam.bhaladhare@gmail.com>
2017-10-30 16:51:51 +0000

    Done

--- q05_lasso/build.py
+++ q05_lasso/build.py
@@ -10,4 +10,19 @@ np.random.seed(9)
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
 
+from math import sqrt
 # Write your solution here
+def lasso(alpha_val=0.01):
+    lasso_model=Lasso(alpha=alpha_val,normalize=True, random_state=9)
+
+    # fit the model on one set of data
+    lasso_model.fit(X_train, y_train)
+
+    # evaluate the model on the second set of data
+    y_pred = lasso_model.predict(X_test)
+    # print y_pred
+    y2 = lasso_model.predict(X_train)
+    rmse2= sqrt(mean_squared_error(y_test, y_pred))
+    rmse1= sqrt(mean_squared_error(y_train, y2))
+
+    return rmse1,rmse2
