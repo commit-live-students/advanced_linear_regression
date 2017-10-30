c3fc8e84a3f143eddf81e432c8bab30d89f7f01c
pritambhaladhare <pritam.bhaladhare@gmail.com>
2017-10-30 16:45:54 +0000

    Done

--- q02_Max_important_feature/build.py
+++ q02_Max_important_feature/build.py
@@ -3,8 +3,6 @@ from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 import numpy as np
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
-
-
 # Write your code here
 # def Max_important_feature(data_set,target_variable='SalePrice',n=4):
 #     k = n
--- q04_ridge/build.py
+++ q04_ridge/build.py
@@ -5,10 +5,17 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 np.random.seed(9)
-
+from math import sqrt 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
 
 # Write your solution here
+def ridge(alp=0.01):
+    ridge_model=Ridge(alpha=alp, random_state=9,normalize=True)
+    ridge_model.fit(X_train, y_train)
+    # evaluate the model on the second set of data
+    y_pred = ridge_model.predict(X_test)
+    y2 = ridge_model.predict(X_train)
 
+    return sqrt(mean_squared_error(y_train, y2)),sqrt(mean_squared_error(y_test, y_pred))
