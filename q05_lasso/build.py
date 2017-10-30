34f56415a6abeb40d590a65e8e1b6ca7291c6c54
gaurav-barclays <gaurav.simply@gmail.com>
2017-10-30 04:22:55 +0000

    Done

--- q05_lasso/build.py
+++ q05_lasso/build.py
@@ -3,29 +3,16 @@
 from sklearn.linear_model import Lasso
 import pandas as pd
 import numpy as np
-from math import sqrt
 from sklearn.metrics import mean_squared_error
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
-
-np.random.seed(9)
-
 def lasso(alpha=0.01):
-    lasso_model=Lasso(alpha=0.01, max_iter=1000, random_state=9,normalize=True)
-    lasso_model.fit(X_train, y_train)
-    y_pred = lasso_model.predict(X_test)
-    y_pred_train = lasso_model.predict(X_train)
-    rmsetest=sqrt(mean_squared_error(y_test, y_pred))
-    rmsetrain=sqrt(mean_squared_error(y_train, y_pred_train))
-    return rmsetrain,rmsetest,lasso_model
-
-rmsetrain,rmsetest,lasso_model=lasso()
-print(rmsetrain)
-print(rmsetest)
-print(type(rmsetrain))
-print(type(rmsetest))
-
-
-# Write your solution here
+    x=Lasso(alpha=alpha, fit_intercept=True, normalize=True, copy_X=True, random_state=9)
+    x.fit(X_train,y_train)
+    y_pred = x.predict(X_train)
+    rmse_train=mean_squared_error(y_train,y_pred)
+    y_pred1=x.predict(X_test)
+    rmse_test=mean_squared_error(y_test,y_pred1)
+    return  np.sqrt(rmse_train),np.sqrt(rmse_test)
