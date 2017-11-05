bd9f55b3122db8b8ce5246939309f98a006d5e4d
samir0719 <sameer_0719@yahoo.com>
2017-11-05 04:18:25 +0000

    Done

--- q06_cross_validation/build.py
+++ q06_cross_validation/build.py
@@ -1,12 +1,14 @@
+# %load q06_cross_validation/build.py
 # Default imports
 from sklearn.model_selection import cross_val_score
 import numpy as np
+from sklearn.linear_model import Ridge
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 
-np.random.seed(9)
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
-
+np.random.seed(9)
+def cross_validation(model,X_train,y_train):
+    score=cross_val_score(model,X_train,y_train,scoring='neg_mean_squared_error',cv=5)
 # Write your solution here
-
-
+    return score.mean()
