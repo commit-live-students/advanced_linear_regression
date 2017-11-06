40d23a3b25dcbdcf53c577650f8e874c26b63367
mritunjaysingh07 <mj.singh07@gmail.com>
2017-11-06 15:00:01 +0000

    Done

--- q02_Max_important_feature/build.py
+++ q02_Max_important_feature/build.py
@@ -3,7 +3,13 @@ from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
+import numpy as np
+
+
+def Max_important_feature(data_set,target_variable = 'SalePrice', n = 4 ):
+    cor = (data_set.corr()[target_variable]).nlargest(n+1)
+    return np.array(cor.drop(target_variable).index)
+
 
 
 # Write your code here
-
--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -3,15 +3,27 @@ from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
 import numpy as np
 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
-def polynomial(power=5,random=9):
+def polynomial(power=5,randomstate = 9):
+    #linear_regression = LinearRegression()
+
+    #extractedData = data_set[:,[2,13,23,24]]
+   # model = make_pipeline([('poly', PolynomialFeatures(degree=5)), ('linear', LinearRegression(fit_intercept=False))])
+    #poly_model = make_pipeline(PolynomialFeatures(5),LinearRegression())
     #model = polynomial()
-    poly_model = make_pipeline(PolynomialFeatures(5),LinearRegression())
-    poly_model.fit_transform(X_train[:, None])
-    return poly_model.predict(np.array([4, 5, 6, 7]).reshape(1, -1))
+
+    linear_regression = LinearRegression()
+    model = Pipeline([("polynomial_features", PolynomialFeatures),("linear_regression", linear_regression)])
+
+    #prediction = model.predict(np.array([4, 5, 6, 7]).reshape(1, -1))
+    return model
+
+
+polynomial(power=5,randomstate = 9)
 
 
 
