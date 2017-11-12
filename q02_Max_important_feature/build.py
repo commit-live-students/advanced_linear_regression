6c7d86c0ef8d1100158cbd8548c5999ee6aec800
vinod-boga <vinodboga17@gmail.com>
2017-11-12 04:26:28 +0000

    Done

--- q02_Max_important_feature/build.py
+++ q02_Max_important_feature/build.py
@@ -7,8 +7,14 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your code here
-def Max_important_feature(data_set,target_variable= "SalePrice",n = 4):
-    c = data_set.corr()
-    Correlation = c[c[target_variable] < 1][target_variable].sort_values(ascending=False).head(n).index.values
-    
-    return Correlation
+#def Max_important_feature(data_set, target_varilable='SalePrice', n=4):
+    #c = data_set.corr()
+    #Correlation = c[c[target_varilable] < 1][target_varilable].sort_values(ascending=False).head(n).index.values
+
+    #return np.array(Correlation)
+def Max_important_feature(data_set, target_varilable, n=4):
+    d = {}
+    for col in data_set.columns.tolist():
+        d[col] = data_set[col].corr(data_set[target_varilable])
+    del d[target_varilable]
+    return np.array(sorted(d, key=d.get, reverse=True)[:n], dtype=object)
