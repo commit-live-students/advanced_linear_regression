58674c2ed137229dd41a2e5c0a0f5a2b565da351
rohanhg91 <rohanhgupta91@gmail.com>
2017-11-14 00:10:46 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -10,6 +10,3 @@ def load_data(path,test_size=0.33,random_state=9):
     X = data.drop(['SalePrice'],axis=1)
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = random_state, train_size=1 - test_size)
     return data,X_train,X_test, y_train, y_test
-
-
-# Write your solution here
