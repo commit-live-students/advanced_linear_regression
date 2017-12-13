4fa8776af52848656b0001cbd5e1f73c6244650f
Bijit Deka <bijitdeka23@gmail.com>
2017-12-13 17:20:54 +0530

    modified q1, q4, q5 test cases

--- q01_load_data/tests/test_q01_load_data.py
+++ q01_load_data/tests/test_q01_load_data.py
@@ -15,34 +15,46 @@ class TestLoad_data(TestCase):
     args = getargspec(load_data) 
     self.assertEqual(args[3], (0.33, 9), "Expected default values do not match given default values")
 
-  def test_load_data_result_types(self):      # Return data types
-    
+  def test_load_data_X_train_type(self):      # Return data types
     data, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
     self.assertIsInstance(X_train, pandas.core.frame.DataFrame,
                               "Expected data type for return value is `pandas DataFrame`, you are returning %s" % (
                                   type(X_train)))
 
+  def test_load_data_X_test_type(self):      # Return data types
+    data, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
     self.assertIsInstance(X_test, pandas.core.frame.DataFrame,
                               "Expected data type for return value is `pandas DataFrame`, you are returning %s" % (
                                   type(X_test)))
 
+  def test_load_data_y_train_type(self):      # Return data types
+    data, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
     self.assertIsInstance(y_train, pandas.core.series.Series,
                               "Expected data type for return value is `pandas DataFrame`, you are returning %s" % (
                                   type(y_train)))
 
+  def test_load_data_y_test_type(self):      # Return data types
+    data, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
     self.assertIsInstance(y_test, pandas.core.series.Series,
                               "Expected data type for return value is `pandas DataFrame`, you are returning %s" % (
                                   type(y_test)))
 
-  def test_load_data_result_values(self):   # Return data values
-
+  def test_load_data_y_test_values(self):   # Return data values
     data, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
     self.assertEqual(y_test.shape, (456,),
                          "Return value shape does not match expected value")
+
+  def test_load_data_X_train_values(self):   # Return data values
+    data, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
     self.assertEqual(X_train.shape, (923, 34),
                          "Return value shape does not match expected value")
 
+  def test_load_data_y_train_values(self):   # Return data values
+    data, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
     self.assertEqual(y_train.iloc[4], 113000,
                          "Return value does not match expected value")
+
+  def test_load_data_X_test_values(self):   # Return data values
+    data, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
     self.assertEqual(X_test.iloc[5, 4], 1963,
                          "Return value value does not match expected value")
--- q05_lasso/tests/test_q05_lasso.py
+++ q05_lasso/tests/test_q05_lasso.py
@@ -13,17 +13,22 @@ class TestLasso(TestCase):
     args = getargspec(lasso)
     self.assertEqual(args[3], (0.01,), "Expected default values do not match given default values")
 
-  def test_lasso_result_type(self):    # Return type tests
+  def test_lasso_rmse1_type(self):    # Return type tests
     rmse1, rmse2 = lasso(0.01)
     self.assertIsInstance(rmse1, float,
                               "Expected data type for return value is `float`, you are returning %s" % (
                                   type(rmse1)))
+  def test_lasso_rmse2_type(self):    # Return type tests
+    rmse1, rmse2 = lasso(0.01)
     self.assertIsInstance(rmse2, float,
                               "Expected data type for return value is `float`, you are returning %s" % (
                                   type(rmse2)))
 
-  def test_lasso_result_values(self):  # Return value tests
+  def test_lasso_rmse1_values(self):  # Return value tests
     rmse1, rmse2 = lasso(0.01)
     self.assertAlmostEqual(rmse1, 33769.142311968972, places=3)
+
+  def test_lasso_rmse2_values(self):  # Return value tests
+    rmse1, rmse2 = lasso(0.01)
     self.assertAlmostEqual(rmse2, 37838.644447277395, places=3)
 
