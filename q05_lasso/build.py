168e35dd40fc00aa49f27d1f7066a5a06f5c0e45
nazirwa <nazirmail@gmail.com>
2018-01-06 03:36:15 +0000

    Done

--- q05_lasso/build.py
+++ q05_lasso/build.py
@@ -11,4 +11,16 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your solution here
+from sklearn.linear_model import Ridge
+def lasso(alpha=0.01):
+    ridge_model = Lasso(alpha=alpha, normalize=True, random_state=9)
+    m = ridge_model.fit(X_train, y_train)
+    y_pred_train = m.predict(X_train)
+    y_pred_test = m.predict(X_test)
+    RMSE_train = np.sqrt(mean_squared_error(y_train, y_pred_train))
+    RMSE_test = np.sqrt(mean_squared_error(y_test,y_pred_test))
+    return RMSE_train, RMSE_test
 
+# rmsetrain, rmsetest = ridge()
+# print "RMSE Train: {0}:  dtype: {1}".format(rmsetrain,type(rmsetrain))
+# print "RMSE Test: {0}:  dtype: {1}".format(rmsetest,type(rmsetest))
