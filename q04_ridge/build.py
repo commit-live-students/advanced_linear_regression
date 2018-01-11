f22c8dbbd6a6e997d201d1883f69f6b94c724dcc
iamsathishsatz <Sathishbabu.s.g@gmail.com>
2018-01-11 16:34:53 +0000

    Done

--- q04_ridge/build.py
+++ q04_ridge/build.py
@@ -8,8 +8,13 @@ np.random.seed(9)
 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
-
-
 # Write your solution here
+def ridge(alpha):
+    ridgeReg = Ridge(alpha=alpha,max_iter=100000, normalize=True,random_state=9)
+
+    y_pred_train = ridgeReg.fit(X_test, y_test).predict(X_train)
+    y_pred_test = ridgeReg.fit(X_train, y_train).predict(X_test)
+    return ridgeReg, np.sqrt(mean_squared_error(y_train, y_pred_train)),np.sqrt(mean_squared_error(y_test, y_pred_test))
 
 
+print ridge(0.01)
--- q05_lasso/build.py
+++ q05_lasso/build.py
@@ -8,7 +8,13 @@ np.random.seed(9)
 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
-
-
 # Write your solution here
+def lasso(alpha):
+    lassoReg = Lasso(alpha=alpha,normalize=True,random_state=9)
 
+    y_pred_train = lassoReg.fit(X_test, y_test).predict(X_train)
+    y_pred_test = lassoReg.fit(X_train, y_train).predict(X_test)
+    return lassoReg, np.sqrt(mean_squared_error(y_train, y_pred_train)),np.sqrt(mean_squared_error(y_test, y_pred_test))
+
+
+print lasso(0.01)
