e11b4071a0bbb8db92026ce9fff15856aacb0553
ms10031990 <ms10031990@yahoo.com>
2018-01-19 18:23:05 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -2,9 +2,12 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-
 path = 'data/house_prices_multivariate.csv'
 
+def load_data(path, test_size=0.33, Random_state=9):
+    df = pd.read_csv(path)
+    X = df.iloc[:,:-1]
+    y = df['SalePrice']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=Random_state, test_size=test_size)
 
-# Write your solution here
-
+    return df,X_train, X_test, y_train, y_test
