3f3b189af1d6cec8d6376646056bd4ea7234f7d0
vp999 <patilvish@gmail.com>
2018-01-20 16:38:10 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -7,4 +7,10 @@ path = 'data/house_prices_multivariate.csv'
 
 
 # Write your solution here
-
+def load_data(path,test_size=0.33,random_state=9):
+    df = pd.read_csv(path)
+    X = df.iloc[:,:-1]
+    y = df['SalePrice']
+    X_train, X_test, y_train, y_test =train_test_split(X,y, random_state=random_state, train_size=(1-test_size))
+    return df,X_train, X_test, y_train, y_test
+#print(load_data (path,0.33))
