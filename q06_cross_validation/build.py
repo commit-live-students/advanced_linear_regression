f760e4f1a6e72963fcee0ee3db67f1420609c893
vp999 <patilvish@gmail.com>
2018-01-25 03:16:19 +0000

    Done

--- q06_cross_validation/build.py
+++ q06_cross_validation/build.py
@@ -8,6 +8,17 @@ np.random.seed(9)
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
 # Write your solution here
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
 
+def cross_validation (model, X,y):
+    scores=cross_val_score(estimator=model, X=X, y=y, cv=5, scoring=('neg_mean_squared_error')) # 5 fold
+    return scores.mean()
 
+#print 'ridge'
+#model = Ridge(0.1, normalize = True, random_state= 9)
+#print cross_validation(model, X_train,y_train)
 
+#print 'lasso'
+#model = Lasso(0.1, normalize = True, random_state= 9)
+#print cross_validation(model, X_train,y_train)
