07dfaff71a2645777718f9feac17c325ea8d1a24
chiragpujari <chg.p786@gmail.com>
2018-02-04 18:28:08 +0000

    Done

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -7,5 +7,10 @@ from sklearn.linear_model import LinearRegression
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
-
 # Write your solution here
+def polynomial(power=5,r_state=9):
+    cor=data_set.corr()['SalePrice']
+    features=cor.sort_values(ascending=False)[1:5].index.values
+    poly_model=make_pipeline(PolynomialFeatures(degree=power,include_bias=False,interaction_only=False),LinearRegression(copy_X=True,fit_intercept=True,n_jobs=1,normalize=False))
+    poly_model.fit(X_train[features],y_train)
+    return poly_model
