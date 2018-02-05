023cb749fe60db3d62b6afbedee6e9e2c1212d36
djocz <jocelynd789@gmail.com>
2018-02-05 12:16:35 +0000

    Done

--- q04_ridge/build.py
+++ q04_ridge/build.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
+from scipy import sqrt
+
 np.random.seed(9)
 
 # We have already loaded the data for you
@@ -11,5 +13,11 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your solution here
-
-
+def ridge(alpha = 0.01):
+    clf = Ridge(alpha=alpha,normalize=True,random_state=9)
+    clf.fit(X_train,y_train)
+    y_pred1 = clf.predict(X_test)
+    y_pred = clf.predict(X_train)
+    rmse2 = sqrt(mean_squared_error(y_pred=y_pred1,y_true=y_test))
+    rmse1 = sqrt(mean_squared_error(y_pred=y_pred,y_true=y_train))
+    return rmse1,rmse2
