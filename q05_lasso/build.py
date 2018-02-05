1235c3152f3619ef7dcc3b3b67e993d20018cfa9
djocz <jocelynd789@gmail.com>
2018-02-05 12:20:45 +0000

    Done

--- q05_lasso/build.py
+++ q05_lasso/build.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
+from scipy import sqrt
 np.random.seed(9)
 
 # We have already loaded the data for you
@@ -11,4 +12,11 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your solution here
-
+def lasso(alpha=0.01):
+    clf = Lasso(alpha=alpha,normalize=True,random_state=9)
+    clf.fit(X_train,y_train)
+    y_pred1 = clf.predict(X_train)
+    y_pred2 = clf.predict(X_test)
+    rmse1 = sqrt(mean_squared_error(y_pred=y_pred1,y_true=y_train))
+    rmse2 = sqrt(mean_squared_error(y_pred=y_pred2,y_true=y_test))
+    return rmse1,rmse2
