0baf6d4e8e1ba925a8838f3d9568cd7e0fde1b0c
bhushan45 <bhushanshewale45@gmail.com>
2018-02-16 04:31:31 +0000

    Done

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -9,3 +9,9 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your solution here
+def polynomial(power=5,r_state=9):
+    cor=data_set.corr()['SalePrice']
+    features=cor.sort_values(ascending=False)[1:5].index.values
+    poly_model=make_pipeline(PolynomialFeatures(degree=power,include_bias=False,interaction_only=False),LinearRegression(copy_X=True,fit_intercept=True,n_jobs=1,normalize=False))
+    poly_model.fit(X_train[features],y_train)
+    return poly_model
