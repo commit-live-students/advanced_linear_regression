bd377b95364e2eb8a5a70812b79e118474c5da45
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2018-02-22 10:09:23 +0000

    Done

--- q06_cross_validation/build.py
+++ q06_cross_validation/build.py
@@ -4,10 +4,14 @@ import numpy as np
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 
 np.random.seed(9)
-# We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
-# Write your solution here
 
+# Write your solution here
+def cross_validation(model, X, y):
+    scores = cross_val_score(model, X_train, y_train, scoring='neg_mean_squared_error', cv=5)
+    return scores.mean()
+
+#cross_validation(Ridge(alpha=0.1),X_train,y_train)
 
 
