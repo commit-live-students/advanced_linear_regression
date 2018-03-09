3d02ddc1767f3233628d8075ebff391dd8361ef4
ga-ashlin-dev <ashlin+78@greyatom.com>
2018-03-09 09:39:36 +0000

    Done

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -1,3 +1,4 @@
+# %load q03_polynomial/build.py
 # Default imports
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 from sklearn.preprocessing import PolynomialFeatures
@@ -9,3 +10,20 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your solution here
+
+# Default imports
+from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LinearRegression
+
+data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
+
+# Write your solution here
+def polynomial(power=5, random_state=9):
+    Poly_Xtrain = X_train[['OverallQual', 'GrLivArea', 'GarageCars', 'GarageArea']]
+    Poly_ytrain = y_train
+    poly_features = make_pipeline(PolynomialFeatures(power, include_bias=False), LinearRegression())
+    model = poly_features.fit(Poly_Xtrain, Poly_ytrain)
+    return model
+
--- q04_ridge/build.py
+++ q04_ridge/build.py
@@ -1,15 +1,41 @@
+# %load q04_ridge/build.py
 # Default imports
 from sklearn.linear_model import Ridge
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
-np.random.seed(9)
 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
+np.random.seed(9)
+
 
 # Write your solution here
 
+
+# Default imports
+from sklearn.linear_model import Ridge
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
+
+np.random.seed(9)
+
+data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
+
+
+def ridge(alpha=0.01):
+    ridge = Ridge(alpha=alpha, normalize=True, random_state=9)
+    ridge.fit(X_train, y_train)
+    predict_train = ridge.predict(X_train)
+    predict_train = pd.DataFrame(predict_train, columns=['Ridge_predict'])
+    rmse1 = np.sqrt(mean_squared_error(y_train, predict_train))
+
+    predict_test = ridge.predict(X_test)
+    predict_test = pd.DataFrame(predict_test, columns=['Ridge_predict'])
+    rmse2 = np.sqrt(mean_squared_error(y_test, predict_test))
+    return rmse1, rmse2
 
