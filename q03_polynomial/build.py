bca91df02e27ab2f0979299cfc929b1bb9931c45
kvin15 <vinayakkhamkar@gmail.com>
2018-03-16 13:50:41 +0000

    Done

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -9,3 +9,8 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your solution here
+def polynomial(power=5,rand_state=9):
+    X_train_val = X_train[['OverallQual','GrLivArea','GarageCars','GarageArea']]
+    poly_regression_model = make_pipeline(PolynomialFeatures(degree=power,include_bias=False), LinearRegression())
+    poly_regression_model.fit(X_train_val, y_train)
+    return poly_regression_model
