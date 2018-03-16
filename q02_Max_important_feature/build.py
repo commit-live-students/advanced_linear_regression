6588049a7a526686e1ac4767e53944e76dfd5852
HerambD <heramb.dharmadhikari@gmail.com>
2018-03-16 07:28:13 +0000

    Done

--- q02_Max_important_feature/build.py
+++ q02_Max_important_feature/build.py
@@ -6,3 +6,10 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your code here
+def Max_important_feature(data_set, target_varilable='SalePrice', n=4):
+    import numpy as np
+    d = {}
+    for col in data_set.columns.tolist():
+        d[col] = data_set[col].corr(data_set[target_varilable])
+    del d[target_varilable]
+    return np.array(sorted(d, key=d.get, reverse=True)[:n], dtype=object)
