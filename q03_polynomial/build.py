3f2ac681cfc96246bced3fce3e2060361d9c0387
axc888 <jha.ashish43@gmail.com>
2018-03-17 15:18:18 +0000

    Done

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -6,6 +6,8 @@ from sklearn.linear_model import LinearRegression
 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
-
-
-# Write your solution here
+def polynomial(power=5,Random_state=9):
+    X_train_var=X_train[['OverallQual','GrLivArea','GarageCars','GarageArea']]
+    poly_model = make_pipeline(PolynomialFeatures(power,include_bias=False),LinearRegression())
+    poly_model.fit(X_train_var, y_train)
+    return poly_model
--- q04_ridge/build.py
+++ q04_ridge/build.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
+import math
 np.random.seed(9)
 
 # We have already loaded the data for you
@@ -11,5 +12,13 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your solution here
-
-
+def ridge(alpha=0.01):
+    ridge_model=Ridge(alpha, normalize=True)
+    ridge_model.fit(X_train,y_train)
+    y_train_pred=ridge_model.predict(X_train)
+    y_test_pred=ridge_model.predict(X_test)
+    mse_train = mean_squared_error(y_train,y_train_pred)
+    mse_test = mean_squared_error(y_test,y_test_pred)
+    rmse_train=math.sqrt(mean_squared_error(y_train,y_train_pred))
+    rmse_test=math.sqrt(mean_squared_error(y_test,y_test_pred))
+    return rmse_train,rmse_test
