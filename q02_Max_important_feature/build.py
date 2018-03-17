b784058290785a40c79da8d270a4006e36e81581
axc888 <jha.ashish43@gmail.com>
2018-03-17 13:31:38 +0000

    Done

--- q02_Max_important_feature/build.py
+++ q02_Max_important_feature/build.py
@@ -7,5 +7,13 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 # Write your code here
 def Max_important_feature(data_set, target_variable="SalePrice", n=4):
-    cor = data_set.corr()[target_variable]
-    return cor.sort_values(ascending=False)[1:n + 1].index.values
+    # cor = data_set.corr()[target_variable]
+    # return cor.sort_values(ascending=False)[1:n + 1].index.values
+    listcorr=[]
+    listcol=[]
+    for col in data_set.columns:
+#         print('{}  {}'.format(col,df['SalePrice'].corr(df[col])))
+        listcorr.append(data_set[target_variable].corr(data_set[col]))
+        listcol.append(col)
+    iowacorr=dict(zip(listcol, listcorr))
+    return sorted(iowacorr, key=iowacorr.get, reverse=True)[1:n+1]
