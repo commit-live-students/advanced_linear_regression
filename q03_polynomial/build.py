9c51d62b4890f68f6c743a27aca5c7047ea96ce9
PBPatil <theprasadpatil@gmail.com>
2018-03-19 15:05:56 +0000

    Done

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -3,9 +3,8 @@ from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
-
+import numpy as np
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
-
-
-# Write your solution here
+def polynomial(power=5,random_state=9):
+    pf = PolynomialFeatures(degree=power,)
--- q04_ridge/build.py
+++ q04_ridge/build.py
@@ -8,8 +8,13 @@ np.random.seed(9)
 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
-
+def ridge(alpha=0.01):
+    l2 = Ridge(alpha=alpha,normalize=True,random_state=9)
+    l2.fit(X_train,y_train)
+    y_pred1 = l2.predict(X_train)
+    rmse_train = np.sqrt(mean_squared_error(y_train,y_pred1))
+    y_pred2 = l2.predict(X_test)
+    rmse_test = np.sqrt(mean_squared_error(y_test, y_pred2))
+    return rmse_train,rmse_test
 
 # Write your solution here
-
-
