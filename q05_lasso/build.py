b8839dbfb35dce0ea44ba7f49812e45a2ce8f24a
PBPatil <theprasadpatil@gmail.com>
2018-03-19 15:10:18 +0000

    Done

--- q05_lasso/build.py
+++ q05_lasso/build.py
@@ -11,4 +11,11 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your solution here
-
+def lasso(alpha=0.01):
+    l1 = Lasso(alpha=alpha,normalize=True,random_state=9)
+    l1.fit(X_train,y_train)
+    y_pred1 = l1.predict(X_train)
+    rmse_train = np.sqrt(mean_squared_error(y_train,y_pred1))
+    y_pred2 = l1.predict(X_test)
+    rmse_test = np.sqrt(mean_squared_error(y_test, y_pred2))
+    return rmse_train,rmse_test
