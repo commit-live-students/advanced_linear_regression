8c14a3cca63780cb6c4553c7ed714562f67a84aa
TheSarang <narkhedesarang@gmail.com>
2018-03-20 11:00:07 +0000

    Done

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -1,3 +1,4 @@
+# %load q03_polynomial/build.py
 # Default imports
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 from sklearn.preprocessing import PolynomialFeatures
@@ -7,5 +8,10 @@ from sklearn.linear_model import LinearRegression
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
+def polynomial(power=5, Random_state=9):
+    poly_model = make_pipeline(PolynomialFeatures(power, include_bias=False), LinearRegression())
+    poly_model.fit(X_train.loc[:,['OverallQual','GrLivArea','GarageCars','GarageArea']], y_train)
+    return poly_model
+polynomial()
 
-# Write your solution here
+
--- q04_ridge/build.py
+++ q04_ridge/build.py
@@ -1,15 +1,23 @@
+# %load q04_ridge/build.py
 # Default imports
 from sklearn.linear_model import Ridge
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
-np.random.seed(9)
-
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import PolynomialFeatures
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
-
-# Write your solution here
+np.random.seed(9)
+def ridge(alpha=0.01):
+    ridge = Ridge(alpha=alpha, normalize=True, random_state=9)
+    ridge.fit(X_train, y_train)
+    y_pred1 = ridge.predict(X_train)    
+    rmse_train = np.sqrt(mean_squared_error(y_train, y_pred1))
+    y_pred2 = ridge.predict(X_test)
+    rmse_test = np.sqrt(mean_squared_error(y_test, y_pred2))
+    return (rmse_train,rmse_test)
 
 
