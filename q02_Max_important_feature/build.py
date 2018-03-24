6894cb6a76db2d5fe24dfd22ad53143681ac6eed
nikhilcss97 <nikhil.css97@gmail.com>
2018-03-24 09:50:25 +0000

    Done

--- q02_Max_important_feature/build.py
+++ q02_Max_important_feature/build.py
@@ -6,3 +6,28 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your code here
+def Max_important_feature(data_set,target_variable='SalePrice',n=4):
+    listcorr = abs(data_set.corr().iloc[-1,:])
+    sampledict = {}
+    cols=data_set.corr().columns
+    it=0
+    for j in listcorr:
+
+        sampledict[j] = cols[it]
+        it+=1
+
+    listcorr = listcorr.sort_values(ascending=False)
+
+    final=[]
+    for i in range(n+1):
+        if i==0:
+            continue
+        final.append(sampledict[listcorr[i]])
+
+    return final
+
+
+
+
+#print(Max_important_feature(data_set,"SalePrice"))
+#print(abs(data_set.corr().iloc[-1,:]).sort_values(ascending=False))
