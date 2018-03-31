6c34d9c71d00a3bdec3ad2d7d9e5f0666d487774
RohinSequeira <sequeira.rohin@gmail.com>
2018-03-31 13:55:27 +0000

    Done

--- q02_Max_important_feature/build.py
+++ q02_Max_important_feature/build.py
@@ -6,3 +6,6 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your code here
+def Max_important_feature(data_set, target_variable="SalePrice", n=4):
+    correlation = data_set.corr()[target_variable]
+    return correlation.sort_values(ascending=False)[1:n + 1].index.values
