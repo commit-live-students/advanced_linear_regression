a70b5d2fa381caaaefe35e1b57169283a249bf41
Abhishek Subramanian <abhisheksubu92@gmail.com>
2018-04-03 09:45:08 +0530

    Update test_q04_ridge.py

--- q04_ridge/tests/test_q04_ridge.py
+++ q04_ridge/tests/test_q04_ridge.py
@@ -16,21 +16,21 @@ class TestRidge(TestCase):
     self.assertEqual(args[3], (0.01,), "Expected default values do not match given default values")
 
   def test_ridge_rmse1_type(self):    # Return type tests
-    rmse1, rmse2 = ridge(0.01)
+    rmse1, rmse2,_ = ridge(0.01)
     self.assertIsInstance(rmse1, float,
                               "Expected data type for return value is `float`, you are returning %s" % (
                                   type(rmse1)))
 
   def test_ridge_rmse2_type(self):    # Return type tests
-    rmse1, rmse2 = ridge(0.01)
+    rmse1, rmse2,_ = ridge(0.01)
     self.assertIsInstance(rmse2, float,
                               "Expected data type for return value is `float`, you are returning %s" % (
                                   type(rmse2)))
 
   def test_ridge_rmse1_values(self):      # Return value tests
-    rmse1, rmse2 = ridge(0.01)
+    rmse1, rmse2,_ = ridge(0.01)
     self.assertAlmostEqual(rmse1, 33775.6544815, places=3)
 
   def test_ridge_rmse2_values(self):      # Return value tests
-    rmse1, rmse2 = ridge(0.01)
+    rmse1, rmse2,_ = ridge(0.01)
     self.assertAlmostEqual(rmse2, 37702.0033295, places=3)
