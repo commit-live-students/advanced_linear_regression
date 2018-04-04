9d60c5bc4dcd144f41a07834cf7b97f02f93cf7d
santoshmayekar <santosh.mayekar@outlook.com>
2018-04-04 16:39:26 +0000

    Done

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -1,11 +1,22 @@
-# Default imports
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
 
-# We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
+cor=data_set.corr()['SalePrice']
+features=cor.sort_values(ascending=False)[1:5].index.values
+poly =PolynomialFeatures(5,include_bias=False)
+X_transform = poly.fit_transform(X_train[features])
+X_test_new = poly.fit_transform(X_test[features])
 
-# Write your solution here
+def polynomial(power=5,r_state=9):
+    lin_regressor = LinearRegression()
+    lin_regressor.fit(X_transform,y_train)
+    return lin_regressor
+
+y_pred = polynomial(power=5,r_state=9).predict(X_test_new)
+print(y_pred.shape)
+
+
--- q04_ridge/build.py
+++ q04_ridge/build.py
@@ -1,15 +1,25 @@
+# %load q04_ridge/build.py
 # Default imports
 from sklearn.linear_model import Ridge
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import PolynomialFeatures
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
-np.random.seed(9)
 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
+n=np.random.seed(9)
 
-# Write your solution here
+def ridge(a=0.01):
+    model = Ridge(alpha=a,normalize=True,random_state=n ).fit(X_train, y_train)
+    y_train_pred=model.predict(X_train)
+    rmse1 = np.sqrt(mean_squared_error(y_train,y_train_pred))
+    y_pred_test = model.predict(X_test)
+    rmse2 = np.sqrt(mean_squared_error(y_test, y_pred_test))
+    return rmse1,rmse2
+
+ridge(a=0.01)
 
 
