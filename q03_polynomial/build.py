c7197251af9597d87957a92354188a842c72514b
rushabh-mehta <rushabh123@gmail.com>
2018-09-17 15:28:23 +0000

    Done

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -1,11 +1,26 @@
+# %load q03_polynomial/build.py
 # Default imports
+import pandas as pd
+from sklearn.model_selection import train_test_split
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
-
+import numpy as np
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
-
+#'OverallQual','GrLivArea','GarageCars','GarageArea'
+#'GarageArea','GarageCars','GrLivArea','OverallQual'
 # Write your solution here
+def polynomial(power=5,random_state=9):
+    model = make_pipeline(PolynomialFeatures(5, include_bias=False),LinearRegression())
+    df = pd.read_csv('data/house_prices_multivariate.csv')
+    X = df.iloc[:,:-1]
+    y = df['SalePrice']
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.33,random_state=9)
+    X_train1=X_train[['OverallQual','GrLivArea','GarageCars','GarageArea']]
+    model.fit(X_train1,y_train)
+    return model
+
+
