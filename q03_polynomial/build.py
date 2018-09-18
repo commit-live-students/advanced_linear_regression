a328a573bdff080b4fb6deadfc090a598bdae414
rajeshbrid <rajesh_brid@hotmail.com>
2018-09-18 14:25:18 +0000

    Done

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -1,5 +1,9 @@
+# %load q03_polynomial/build.py
 # Default imports
+import pandas as pd
+import numpy as np
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
@@ -7,5 +11,27 @@ from sklearn.linear_model import LinearRegression
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
-
 # Write your solution here
+
+def polynomial(power=5, random_state=9):
+    model = make_pipeline(PolynomialFeatures(5, include_bias=False),LinearRegression())
+    df = pd.read_csv('data/house_prices_multivariate.csv')
+    
+    X = df.iloc[:,:-1]
+    y = df['SalePrice']
+    
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.33,random_state=9)
+    X_train1 = X_train[['OverallQual','GrLivArea','GarageCars','GarageArea']]
+    
+    #print (X_train1)
+    
+    model.fit(X_train1,y_train)
+    
+    #print (model.fit(X_train1,y_train))
+    
+    return model
+
+#polynomial(power=5, random_state=9)
+
+
+
