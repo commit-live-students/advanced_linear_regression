ead824e911799e15268834c3dc27627cdd9957df
ShardulJ <Shardul2607@gmail.com>
2018-09-20 06:51:58 +0000

    Done

--- q02_Max_important_feature/build.py
+++ q02_Max_important_feature/build.py
@@ -1,8 +1,16 @@
+# %load q02_Max_important_feature/build.py
 # Default imports
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
+# target_variable = data_set['SalePrice']
+def Max_important_feature(data_set, target_variable =  'SalePrice', n = 4 ):
+    corr = data_set.corr()['SalePrice']
+    corr = corr.sort_values(ascending = False).drop(labels = 'SalePrice').nlargest(n)
+    Correlation = corr.index.tolist()
+    return Correlation
+Max_important_feature(data_set, y_test, 4 )
 
 
-# Write your code here
+
