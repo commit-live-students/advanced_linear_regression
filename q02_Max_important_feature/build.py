3d37b271e25eb97a9f4dbc263cac37c5a7b93a07
Akash Desarda <desardaakash@gmail.com>
2018-09-25 11:27:24 +0530

    Update build.py

--- q02_Max_important_feature/build.py
+++ q02_Max_important_feature/build.py
@@ -7,12 +7,9 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your code here
-def Max_important_feature(data_set,y='SalePrice',n=4):
-    features=[]
-    corr_values = data_set.corr(method='pearson')['SalePrice']
-    corr_values = corr_values.abs().sort_values()
-    features = corr_values.index.tolist()
-    return features[len(features)-n-1:len(features)-1][::-1]
-Max_important_feature(data_set,y='SalePrice',n=4)
-
-
+def Max_important_feature(data_set, a, n=5):
+    corr_values = data_set.corr(method='pearson')[a]
+    b = corr_values.nlargest(n).index.tolist()
+    b.remove('SalePrice')
+    return b
+Max_important_feature(data_set,a = 'SalePrice', n=5)
