395ecd7113f5880341a0d971b31fd49306a1571a
Akashdesarda <desardaakash@gmail.com>
2018-09-26 18:03:30 +0000

    Done

--- q05_lasso/build.py
+++ q05_lasso/build.py
@@ -1,14 +1,24 @@
+# %load q05_lasso/build.py
 # Default imports
 from sklearn.linear_model import Lasso
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
-np.random.seed(9)
 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
+np.random.seed(9)
+
 
 # Write your solution here
+def lasso(alpha=0.01):
+    model = Lasso(alpha=alpha, normalize = True, random_state=9)
+    model.fit(X_train,y_train)
+    y_pred_train = model.predict(X_train)
+    y_pred_test = model.predict(X_test)
+    return mean_squared_error(y_train,y_pred_train)**0.5,mean_squared_error(y_test,y_pred_test)**0.5
+lasso()
+
 
