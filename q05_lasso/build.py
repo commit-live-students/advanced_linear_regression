802b2a6990a8d0d8f841129c01d71b07e3db4342
EminentNeo <aniketjagtap18@gmail.com>
2018-09-30 03:49:41 +0000

    Done

--- q05_lasso/build.py
+++ q05_lasso/build.py
@@ -1,6 +1,6 @@
 # %load q05_lasso/build.py
 # Default imports
-from sklearn import linear_model
+from sklearn.linear_model import Lasso
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
@@ -8,18 +8,19 @@ from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 from math import sqrt
 
 # We have already loaded the data for you
-a = 0.01
-def lasso(alpha = 0.01 ):
+#a = 0.01
+def lasso(a_1 = 0.01):
     data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
     #np.random.seed(9)
-    lasso_model=linear_model.Lasso(alpha = alpha)
+    lasso_model=Lasso(alpha = a_1,normalize =True, random_state=9)
     Model = lasso_model.fit(X_train, y_train)
     y_pred_1 = lasso_model.predict(X_train)
     y_pred_2 = lasso_model.predict(X_test)
-    rmse1 = float(sqrt(mean_squared_error(y_pred_1, y_train)))
-    rmse2 = float(sqrt(mean_squared_error(y_pred_2, y_test)))
-    return rmse1, rmse2, Model
-lasso(0.01)
+    rmse1 = float('%.3f' % sqrt(mean_squared_error(y_pred_1, y_train)))
+    rmse2 = float('%.3f' % sqrt(mean_squared_error(y_pred_2, y_test)))
+    return  rmse1, rmse2
+lasso()
+
 
 
 
