b2479fb0c69a642a18b48cd088a80e4d9d1a03b9
Tusharsharma86 <tusharsharma86@gmail.com>
2018-10-10 12:24:19 +0000

    Done

--- q02_Max_important_feature/build.py
+++ q02_Max_important_feature/build.py
@@ -1,8 +1,22 @@
+# %load q02_Max_important_feature/build.py
 # Default imports
+import numpy as np
+import pandas as pd
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
 
-# Write your code here
+# Function to import import numerical feature which have highest corr
+def Max_important_feature(data_set,target_variable = 'SalePrice' , n = 4):
+    col = data_set.corr().nlargest(n=(n+1),columns=target_variable)[target_variable].index[1:]
+    return list(col)
+
+Max_important_feature(data_set,target_variable = 'SalePrice' , n = 4)
+
+
+
+
+
+
