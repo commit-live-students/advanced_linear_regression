99fc275775e4dcbf0fd8a084e23997700c2c67a3
Abhimanyu22 <link2manyu@hotmail.com>
2018-10-31 16:57:12 +0000

    Done

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -1,11 +1,21 @@
-# Default imports
+import pandas as pd
+import numpy as np
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
 
-# We have already loaded the data for you
-data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
+data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv', 0.33, 9)
+
+def polynomial(power = 5, randomst=9):
+    cols = data_set.corr().nlargest(5, 'SalePrice')['SalePrice'].index
+    poly_model = make_pipeline(PolynomialFeatures(degree=power, include_bias=False), LinearRegression())
+    poly_model.fit(X_train[cols[1:]], y_train)
+    
+    return poly_model
+
+polynomial()
 
 
-# Write your solution here
+
+
