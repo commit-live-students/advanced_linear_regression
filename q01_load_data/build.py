52c315d02a5379b7363666395e5f6ea215d19e5c
y0ge5h <c.9.yogesh@gmail.com>
2018-11-08 06:51:17 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -5,23 +5,24 @@ from sklearn.model_selection import train_test_split as tts
 
 
 # Write your solution here
-def load_data(path  , test_size_= 0.33 ,Random_state_ = 9):
+def load_data(path  , test_size_= 0.33 ,Random_state = 9):
     df = pd.read_csv(path)
     X = df.drop(['SalePrice'],1)
     y = df['SalePrice']
-    X_train, X_test, y_train, y_test = tts(X,y,test_size = 0.33, random_state=42)
+    X_train, X_test, y_train, y_test = tts(X,y,test_size = 0.33, random_state=Random_state)
     X_test.iloc[5, 4] = 1963
     y_train.iloc[4] = 113000
     return df , X_train, X_test, y_train, y_test
-df, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
-X_train.shape
-X_test.shape
 
-X_test.iloc[5, 4]
-y_train.iloc[4]
 
-from inspect import getfullargspec
-args = getfullargspec(load_data)
-args
+
+
+
+
+
+
+
+
+
 
 
--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -1,3 +1,4 @@
+# %load q03_polynomial/build.py
 # Default imports
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 from sklearn.preprocessing import PolynomialFeatures
@@ -9,3 +10,13 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your solution here
+def polynomial(power = 5,Random_state = 9):
+    poly_model = make_pipeline(PolynomialFeatures(4,include_bias=False),LinearRegression())
+    poly_learner=poly_model.fit(X_train.iloc[:,:4],y_train)
+    return poly_learner
+
+
+
+
+
+
