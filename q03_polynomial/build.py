a98e13aab6dbdd2d8eb50feb04bfba8571bc59d4
y0ge5h <c.9.yogesh@gmail.com>
2018-11-08 06:53:03 +0000

    Done

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -11,12 +11,14 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 # Write your solution here
 def polynomial(power = 5,Random_state = 9):
-    poly_model = make_pipeline(PolynomialFeatures(4,include_bias=False),LinearRegression())
-    poly_learner=poly_model.fit(X_train.iloc[:,:4],y_train)
+    poly_model = make_pipeline(PolynomialFeatures(power,include_bias=False),LinearRegression())
+    cols = ['OverallQual','GrLivArea','GarageCars','GarageArea']
+    poly_learner=poly_model.fit(X_train[cols],y_train)
     return poly_learner
-
-
-
+d = polynomial()
+import numpy as np
+prediction = d.predict(np.array([4, 5, 6, 7]).reshape(1, -1))
+prediction
 
 
 
