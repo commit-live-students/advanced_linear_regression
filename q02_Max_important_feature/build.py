4ebc962a8aceb6b25cb568ad410cf4f0ca3b25b6
tracedence <shoaibrashid887@gmail.com>
2018-11-15 12:13:59 +0000

    Done

--- q02_Max_important_feature/build.py
+++ q02_Max_important_feature/build.py
@@ -1,3 +1,4 @@
+# %load q02_Max_important_feature/build.py
 # Default imports
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
 
@@ -6,3 +7,11 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 
 
 # Write your code here
+def Max_important_feature(data_set, target_variable='SalePrice', n=4):
+    
+    value = data_set.corr()[target_variable].drop(target_variable, axis =0)
+    
+    return value[value > 0.604].index
+Max_important_feature(data_set, target_variable='SalePrice', n=4)
+
+
