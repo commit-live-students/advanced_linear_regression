3843a21e7655c7f0b70024309e468bedb4e041b0
prodigy803 <pushkaraj803@gmail.com>
2018-11-17 04:30:18 +0000

    Done

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -7,21 +7,18 @@ from sklearn.linear_model import LinearRegression,Ridge,Lasso
 from sklearn.pipeline import Pipeline
 import numpy as np
 # We have already loaded the data for you
-data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
+
 
 
 # Write your solution here
 def polynomial(power=5,Random_state=9):
-    #model = make_pipeline(PolynomialFeatures(include_bias=False), LinearRegression())
-    poly = PolynomialFeatures(include_bias=False)
-    linear_reg = LinearRegression()
-    X_train = data_set.loc[:,['OverallQual','GrLivArea','GarageCars','GarageArea']]
-    y_train = data_set.loc[:,'SalePrice']
-    X_train = poly.fit(X_train)
-    y_train = poly.fit(y_train)
-    linear_reg.fit(X_train, y_train)
+    data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv',random_state=Random_state)
+    arr = np.array(data_set.corr()['SalePrice'].sort_values(ascending=False).index)
+    model = make_pipeline(PolynomialFeatures(degree = power, include_bias = False), LinearRegression())
+    
+    model.fit(X_train.loc[:, arr[1:4+1]],y_train)
+    return model
 
-    return linear_reg
 
 
 
