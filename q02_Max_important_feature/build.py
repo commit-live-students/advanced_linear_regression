60e98f030c38099c2735692cce4b3b6c73821640
tracedence <shoaibrashid887@gmail.com>
2018-11-24 11:44:16 +0000

    Done

--- q02_Max_important_feature/build.py
+++ q02_Max_important_feature/build.py
@@ -9,9 +9,12 @@ data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multiv
 # Write your code here
 def Max_important_feature(data_set, target_variable='SalePrice', n=4):
     
-    value = data_set.corr()[target_variable].drop(target_variable, axis =0)
+    value = data_set.corr()['SalePrice'].drop('SalePrice').sort_values(ascending=False)[0:4]
     
-    return value[value > 0.604].index
+    return value.index
+
+
 Max_important_feature(data_set, target_variable='SalePrice', n=4)
+
 
 
