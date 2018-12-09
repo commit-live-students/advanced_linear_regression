9b6593fd72b8728617eeb528da7d0f056ba09b62
dhananjay93 <dhananjay.hawal@gmail.com>
2018-12-09 11:07:50 +0000

    Done

--- q03_polynomial/build.py
+++ q03_polynomial/build.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
 
+
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 X_train = X_train[['OverallQual','GrLivArea','GarageCars','GarageArea']]
@@ -13,12 +14,9 @@ X_test = X_test[['OverallQual','GrLivArea','GarageCars','GarageArea']]
 # Write your solution here
 def polynomial(power=5,Random_state=9):
     # PolynomialFeatures (prepreprocessing)
-    poly = PolynomialFeatures(degree=power,include_bias=False)
-    poly_x = poly.fit_transform(X_train)
-    regressor=LinearRegression()
-    model = regressor.fit(poly_x,y_train)
-    return model
-    
-
+    poly_model = make_pipeline(PolynomialFeatures(power),
+                           LinearRegression())
+    poly_model.fit(X_train,y_train)
+    return poly_model
 
 
