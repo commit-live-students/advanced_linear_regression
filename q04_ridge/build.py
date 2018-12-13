fdcb4dcc216251ddfa10ab7ba75a5c6452f228d8
psicktrick <poojarysaurabh932@gmail.com>
2018-12-13 05:42:25 +0000

    Done

--- q04_ridge/build.py
+++ q04_ridge/build.py
@@ -1,15 +1,28 @@
+# %load q04_ridge/build.py
 # Default imports
 from sklearn.linear_model import Ridge
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from greyatomlib.advanced_linear_regression.q01_load_data.build import load_data
-np.random.seed(9)
 
 # We have already loaded the data for you
 data_set, X_train, X_test, y_train, y_test = load_data('data/house_prices_multivariate.csv')
 
+np.random.seed(9)
+
 
 # Write your solution here
+def ridge(alpha=0.01):
+    ridge_model = Ridge(alpha=alpha, normalize=True, random_state=9)
+    ridge_model.fit(X_train, y_train)
+    y_pred = ridge_model.predict(X_test)
+    test_rmse = np.sqrt(mean_squared_error(y_pred=y_pred, y_true=y_test))
+    y_train_pred = ridge_model.predict(X_train)
+    train_rmse = np.sqrt(mean_squared_error(y_pred=y_train_pred, y_true=y_train))
+    return train_rmse, test_rmse, ridge_model
+
+X_train.head()
+ridge(alpha=0.01)
 
 
